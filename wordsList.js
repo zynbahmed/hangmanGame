30a513a61748aae617dceb180bf37073b12c2572
zynbahmed <201901241@student.polytechnic.bh>
2024-01-19 23:00:35 +0300

    adding words for the wordlist file (5 for now)

---
+++ wordsList.js
@@ -0,0 +1,22 @@
+const wordList = [
+    {
+        word:"Elephant",
+        hint:"Largest land mammal with a trunk"
+    },
+    {
+        word:"Rainbow",
+        hint:"A colorful arc in the sky often seen after rain"
+    },
+    {
+        word:"Lighthouse",
+        hint:"A tall structure with a light to guide ships at sea"
+    },
+    {
+        word:"Pillow",
+        hint:"Soft support for your head while sleeping"
+    },
+    {
+        word:"Turtle",
+        hint:"A slow-moving reptile with a protective shell"
+    }
+]
