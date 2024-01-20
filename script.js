f634a658ac2aacdcd09c92de2055575efd85cf14
zynbahmed <201901241@student.polytechnic.bh>
2024-01-20 16:40:54 +0300

    adding the keyboard buttons

--- script.js
+++ script.js
@@ -43,7 +43,12 @@ const resetGame = () => {
 
 }
 
-
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    kb.appendChild(button);
+    button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+}
 
 getRandomWord()
 playAgainBtn.addEventListener("click", getRandomWord)
