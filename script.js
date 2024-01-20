baaf97fe2484a66bfc7b81a1e2a1e6bcfd1381a9
zynbahmed <201901241@student.polytechnic.bh>
2024-01-20 15:37:40 +0300

    some fixes with names and added the getRandomWord function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const word = document.querySelector('.word')
-const hint = document.querySelector('.hint')
+const wordText = document.querySelector('.word')
+const hintText = document.querySelector('.hint')
 const guesses = document.querySelector('.guesses')
 const kb = document.querySelector('.keyboard')
 const img = document.querySelector('.hangman img')
@@ -9,3 +9,12 @@ const reset = gameEnds.querySelector('.reset')
 let currentWord, correctLtr, wrongGuesses
 const maxGuesses = 6
 
+const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)]
+    currentWord = word
+    hintText.innerText = hint
+    //resetGame();
+}
+
+getRandomWord()
+playAgainBtn.addEventListener("click", getRandomWord)
