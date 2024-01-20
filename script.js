7128eaef6656e3ac1ec8c3d50145ac08e43a4f8e
zynbahmed <201901241@student.polytechnic.bh>
2024-01-20 15:30:54 +0300

    creating the global variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const word = document.querySelector('.word')
+const hint = document.querySelector('.hint')
+const guesses = document.querySelector('.guesses')
+const kb = document.querySelector('.keyboard')
+const img = document.querySelector('.hangman img')
+const gameEnds = document.querySelector('.game-ends')
+const reset = gameEnds.querySelector('.reset')
+
